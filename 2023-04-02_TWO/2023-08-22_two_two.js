88c3c94a2b72d30d4130991f3dd9dd9115691260
Vappiter <vappiter@yandex.ru>
2023-09-14 17:34:20 +0300

    2023-09-14_WORK

--- 2023-04-02_TWO/2023-08-22_two_two.js
+++ 2023-04-02_TWO/2023-08-22_two_two.js
@@ -1,15 +1,13 @@
-const Clothes =
-    [
-    //  {id: 0, name:''},   
-    //  {id: 1, name:''},
-    //  {id: 2, name:''},
-    //  {id: 3, name:''},
-    //  {id: 4, name:''},   
-    ]
+const Clothes =   []
 
-console.log(Clothes[0]);
-Clothes.push({id:5,price:100});
-// Clothes[7].sizes = [40,42,44,46,58,60];
-let varName = prompt('Введите наименование одежды:');
 console.log(Clothes);
-console.log(varName);
+Clothes.push({id:1,name:'ONE', description: 'dONE', sizes:[40,42,44,46,58,60], price:100, available: true});
+Clothes.push({id:2,name:'TWO', description: 'dTWO', sizes:[40,42,44,46,58,60], price:200, available: false});
+Clothes.push({id:3,name:'THREE', description: 'dTHREE', sizes:[40,42,44,46,58,60], price:300, available: true});
+Clothes.push({id:4,name:'FOUR', description: 'dFOUR', sizes:[40,42,44,46,58,60], price:400, available: false});
+Clothes.push({id:5,name:'FIVE', description: 'dFIVE', sizes:[40,42,44,46,58,60], price:500, available: true});
+
+document.write((Clothes));
+
+let varName = prompt ('Тест');
+document.write(varName);
